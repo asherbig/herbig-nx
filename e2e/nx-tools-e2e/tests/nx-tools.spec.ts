588472851edad11b2b9be8176ff8bab8201acaa9
asherbig <aherbigs@gmail.com>
2021-09-21 21:38:00 -0400

    testing spec commands

--- e2e/nx-tools-e2e/tests/nx-tools.spec.ts
+++ e2e/nx-tools-e2e/tests/nx-tools.spec.ts
@@ -1,42 +1,22 @@
 import {
-  checkFilesExist,
   ensureNxProject,
   readJson,
   runNxCommandAsync,
-  uniq,
 } from '@nrwl/nx-plugin/testing';
 describe('nx-tools e2e', () => {
-  it('should create nx-tools', async () => {
-    const plugin = uniq('nx-tools');
-    ensureNxProject('@herbig-nx/nx-tools', 'dist/packages/nx-tools');
-    await runNxCommandAsync(`generate @herbig-nx/nx-tools:nx-tools ${plugin}`);
-
-    const result = await runNxCommandAsync(`build ${plugin}`);
-    expect(result.stdout).toContain('Executor ran');
-  }, 120000);
-
-  describe('--directory', () => {
-    it('should create src in the specified directory', async () => {
-      const plugin = uniq('nx-tools');
-      ensureNxProject('@herbig-nx/nx-tools', 'dist/packages/nx-tools');
-      await runNxCommandAsync(
-        `generate @herbig-nx/nx-tools:nx-tools ${plugin} --directory subdir`
-      );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
-      ).not.toThrow();
-    }, 120000);
-  });
-
   describe('--tags', () => {
     it('should add tags to nx.json', async () => {
-      const plugin = uniq('nx-tools');
       ensureNxProject('@herbig-nx/nx-tools', 'dist/packages/nx-tools');
-      await runNxCommandAsync(
-        `generate @herbig-nx/nx-tools:nx-tools ${plugin} --tags e2etag,e2ePackage`
-      );
+      await runNxCommandAsync(`generate @nrwl/node:application --name=food --tags type:app,scope:food`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=food-ui --tags type:ui,scope:food`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=food-shared --tags type:shared,type:food`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=server --tags type:app,scope:server`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=server-shared --tags type:shared,scope:server`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=core-ui --tags type:ui,scope:core`);
+      await runNxCommandAsync(`generate @nrwl/node:application --name=core-shared --tags type:shared,scope:core`);
       const nxJson = readJson('nx.json');
-      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage']);
+      console.log('============nxJson============', nxJson);
+      expect(nxJson.projects['food'].tags).toEqual(['type:app', 'scope:food']);
     }, 120000);
   });
 });
--- packages/nx-tools/src/executors/build/executor.ts
+++ packages/nx-tools/src/executors/build/executor.ts
@@ -1,7 +1,10 @@
+import { readJsonFile } from '@nrwl/devkit';
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
   console.log('Executor ran for Build', options);
+  const nxJson = readJsonFile('nx.json');
+  console.log('nxJson2 from executor:', nxJson);
   return {
     success: true,
   };
